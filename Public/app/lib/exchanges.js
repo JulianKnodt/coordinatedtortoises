578faed8b4d515654d7e7c113846333990f5f782
JulianKnodt <julianknodt@gmail.com>
2016-09-12 19:23:35 -0700

    Fixed some basic things

--- Public/app/lib/exchanges.js
+++ Public/app/lib/exchanges.js
@@ -5,11 +5,11 @@ window.exchangeData = {};
 //Update exchangeRates queries our server for the current exchange rate data and appends it within the panel for transactions.
 //It's called every minute by the componentDidMount of exchanges.jsx
 var updateExchangeRates = function(){
+  $('.exRates').empty();
   $.ajax({
       url: 'http://localhost:3000/exchange',
       method: 'GET',
       success: (data) => {
-        $('exRates').empty();
         var rates = JSON.parse(data);
         $('.exRates').append('<h3 class="text-center">Rates per 1 Ƀ</h3><br>');
         $('.exRates').append('<table class="table exData"><tr><th>Currency</th><th>Buy Rate</th><th>Sell Rate</th></table>');
